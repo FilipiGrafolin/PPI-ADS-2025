7fc80ba9851dc92f19e1024f99588dfe7ca0a7eb
Filipi Antonio Medeiros Grafolin <filipi.03989@aluno.iffar.edu.br>
2025-06-24 20:38:12 -0300

    Verificando a última posição do vetor e verificando o valor de cada casa do vetor.
    Verificando a última posição do vetor e verificando o valor de cada casa do vetor. For para escrever os números das casas ao contrário para poder realizar os cálculos com potência. OBS.: Verificar potência.

--- paraDecimal.java
+++ paraDecimal.java
@@ -41,14 +41,43 @@ public class paraDecimal {
 
          */
 
-
         System.out.println("Informe o número: ");
         int numeroDigitado = lendoTeclado.nextInt();
 
-        String numeroDig = Integer.toString(numeroDigitado);
+        System.out.println("Informe a base do número: ");
+        int baseDigitada = lendoTeclado.nextInt();
 
-        char[] meuVetor = numeroDig.toCharArray();
-        System.out.printf(Arrays.toString(meuVetor));
+        String numeroDig = Integer.toString(numeroDigitado);
+        int tamanhoString = numeroDig.length();
+
+        char[] vetor = numeroDig.toCharArray();
+        System.out.printf(Arrays.toString(vetor));
+
+        int potencia=0;
+        int ultimaPosicao = (vetor.length)-1;
+        int resultado =0;
+
+        //System.out.println(vetor[ultimaPosicao]);
+        //System.out.printf("ULTIMA POSICAO: %d", ultimaPosicao);
+
+        for (int cont = ultimaPosicao; cont>=0; cont--){
+            potencia++;
+            for (int i = 0 ; i <= potencia ; i++) {
+                resultado *= vetor[cont];
+            }
+            System.out.printf("\nCONT: %d\nVALOR: "+vetor[cont]+"\nPOTENCIA: %d\nRESULTADO %d",cont,potencia,resultado);
+
+            
+            /*if (resultado == 0){
+                resultado = resultado - resultado + 1;
+                System.out.printf("\nRESULTADO: %d",resultado);
+            }else {
+                System.out.printf("\nRESULTADO: %d",resultado);
+            }
+
+             */
+
+        }
 
 
     }
